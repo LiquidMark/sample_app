94f0f26dc0b24a738f6489fc514195f87f582197
Mark Molloy <mark.e.molloy@gmail.com>
2012-02-23 16:52:29 -0600

    Add blueprint stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ module ApplicationHelper
 	
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
+    base_title = "Liquid Autos"
+    if page_title.empty?       
+      base_title                                      
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} | #{page_title}"                 
     end
   end
 
