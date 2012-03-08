1306e20f2815ab07e892e7fd0098cd9dd121a493
Mark Molloy <mark.e.molloy@gmail.com>
2012-03-08 13:20:14 -0600

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page], :per_page => 10)
+  end
 
   def show
     @user = User.find(params[:id])
@@ -38,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end  
+
   private
 
     def signed_in_user
@@ -50,4 +61,8 @@ class UsersController < ApplicationController
       redirect_to(root_path) unless current_user?(@user)
     end
 
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,8 +5,26 @@ describe "Authentication" do
   subject { page }
 
   describe "authorization" do
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }        
+      end
+    end
+    
     describe "for non-signed-in users" do
       let(:user) { Factory(:user) }
+
+      describe "visiting user index" do
+        before { visit users_path }
+        it { should have_selector('title', text: 'Sign in') }
+      end
 
       describe "when attempting to visit a protected page" do
         before do
@@ -82,7 +100,9 @@ describe "Authentication" do
       end
 
       it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Users',    href: users_path) }
+      it { should have_link('Profile',  href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in') }
 
