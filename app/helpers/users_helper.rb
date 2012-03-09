14f00092cf7452c6ab2f99dcb34d4ccbfcf4e126
Mark Molloy <mark.e.molloy@gmail.com>
2012-03-09 09:08:22 -0600

    exercise 9.1.1

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt:user.name, class:"gravatar") 
   end
 	
 end
